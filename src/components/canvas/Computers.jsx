859170bfa49b73994a8fb8c46fda4eceec4fe411
HaderCabrera <hader.cabrera96@gmail.com>
2024-08-18 12:34:27 -0500

    cambiarto eart

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -55,7 +55,7 @@ const ComputersCanvas = () => {
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          autoRotate
+          // autoRotate
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
