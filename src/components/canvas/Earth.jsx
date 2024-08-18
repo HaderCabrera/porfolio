076c44e047386ac629558b18d56f2b1ed4c92ac0
HaderCabrera <hader.cabrera96@gmail.com>
2024-08-18 11:31:54 -0500

    cambiarto eart

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,10 +5,13 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("./planet_eart.glb");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <mesh>
+    <ambientLight intensity={0.5} />
+    <primitive object={earth.scene} scale={2.5} position-y={-3} rotation-y={0} />
+  </mesh>
   );
 };
 
