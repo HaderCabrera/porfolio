49d72a5e0d74d7f521e906f9dcdf401a3157e20f
HaderCabrera <hader.cabrera96@gmail.com>
2024-08-18 11:03:30 -0500

    Cambiando modelo 3d

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,24 +5,15 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./retrofuturistic_computer/scene.gltf");
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor='black' />
-      <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
+      <ambientLight intensity={0.5} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.04 : 0.07}
+        position={isMobile ? [0, -1, -2.2] : [0, -1, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
